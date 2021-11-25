52f036f5d7192074247c735b60edf22a27ad5bd9
Jesega <jesega2000@gmail.com>
2021-11-25 11:12:25 +0100

    Funcionalidad leer()

--- devops/src/devops/MainFunc.java
+++ devops/src/devops/MainFunc.java
@@ -1,7 +1,18 @@
 package devops;
 
+import java.util.Scanner;
+
 public class MainFunc {
+	Scanner scan = new Scanner(System.in);
+	
+	public String leer()
+	{
+		return scan.nextLine();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world");  
 	}
+	
+	
 }
